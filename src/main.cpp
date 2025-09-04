fb72ffaa59e153b3deaab2e67e76a0dbc0914baa
ZakyHermawan <zaky.hermawan9615@gmail.com>
2025-09-04 22:52:03 +0700

    Properly place resource files and read example YAML file
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "ButtonHandler.hpp"
+#include "fkYAML.hpp"
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QFile>
+
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +23,21 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.loadFromModule("gwi", "Main");
+
+    QFile file(":/qt/qml/gwi/resources/example.yml");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        std::cerr << "Error: Could not open file from Qt resources!" << std::endl;
+        return 1;
+    }
+
+    // Read the whole file into a string
+    QTextStream in(&file);
+    std::string content = in.readAll().toStdString();
+    file.close();
+
+    // Now, parse the string content with fkYAML
+    auto node = fkyaml::node::deserialize(content);
+    std::cout << node << std::endl;
 
     return app.exec();
 }
